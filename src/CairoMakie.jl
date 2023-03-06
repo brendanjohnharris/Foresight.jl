1d62ef0d1e0f42e068617cfbd09528a7157227f9
brendanjohnharris <brendanjohnharris@gmail.com>
2023-03-06 16:32:39 +1100

    Up

--- src/CairoMakie.jl
+++ src/CairoMakie.jl
@@ -1,9 +1,8 @@
 using .CairoMakie
 using .Gtk
 
-function drawonto(canvas, figure)
+function drawonto(canvas, scene::CairoMakie.Scene)
     @guarded draw(canvas) do _
-        scene = figure.scene
         resize!(scene, Gtk.width(canvas), Gtk.height(canvas))
         config = CairoMakie.ScreenConfig(1.0,1.0,:good,true,true)
         screen = CairoMakie.Screen(scene, config, Gtk.cairo_surface(canvas))
@@ -11,15 +10,19 @@ function drawonto(canvas, figure)
     end
 end
 
-function gtkshow(f::CairoMakie.Figure; name="CairoMakie", resolution=(1920, 1080), kwargs...)
+function gtkshow(scene::CairoMakie.Scene; name="CairoMakie", resolution=(1920, 1080), kwargs...)
     canvas = @GtkCanvas()
     window = GtkWindow(canvas, name, resolution...; kwargs...)
-    drawonto(canvas, f)
+    drawonto(canvas, scene)
     show(canvas)
 end
+gtkshow(f::CairoMakie.Figure; kwargs...) = gtkshow(f.scene; kwargs...)
 gtkshow(f::CairoMakie.Makie.FigureAxisPlot; kwargs...) = gtkshow(f.figure; kwargs...)
 export gtkshow
 
 Base.show(io::IO, f::CairoMakie.Makie.FigureAxisPlot) = gtkshow(f)
 Base.show(io::IO, f::CairoMakie.Figure) = gtkshow(f)
 Base.show(io::IO, f::CairoMakie.Scene) = gtkshow(f)
+Base.display(f::CairoMakie.Makie.FigureAxisPlot) = gtkshow(f)
+Base.display(f::CairoMakie.Figure) = gtkshow(f)
+Base.display(f::CairoMakie.Scene) = gtkshow(f)
