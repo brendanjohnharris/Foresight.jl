5b6f66100d9fe9f220e3c941c130c2de6f0fa443
brendanjohnharris <brendanjohnharris@gmail.com>
2023-03-13 12:42:17 +1100

    Update figures

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,19 @@ using CairoMakie
 
 @testset "Importall" begin
     @test all(isnothing.(eval.(importall(Foresight))))
-    @test_nowarn save("./demo_default.png", demofigure())
+    @test_nowarn Makie.set_theme!(foresight())
+    @test_nowarn save("./demo_default.png", demofigure(), px_per_unit = 5)
 end
 
 @testset "Demo figure" begin
-    @test_nowarn save("./demo.png", demofigure())
+    @test_nowarn save("./demo.png", demofigure(), px_per_unit = 5)
 
     @test_nowarn Makie.set_theme!(foresight(:dark))
-    @test_nowarn save("./demo_dark.png", demofigure())
+    @test_nowarn save("./demo_dark.png", demofigure(), px_per_unit = 5)
 
     @test_nowarn Makie.set_theme!(foresight(:dark, :transparent))
-    @test_nowarn save("./demo_transparentdark.png", demofigure())
+    @test_nowarn save("./demo_transparentdark.png", demofigure(), px_per_unit = 5)
 
     @test_nowarn Makie.set_theme!(foresight(:serif))
-    @test_nowarn save("./demo_serif.png", demofigure())
+    @test_nowarn save("./demo_serif.png", demofigure(), px_per_unit = 5)
 end
