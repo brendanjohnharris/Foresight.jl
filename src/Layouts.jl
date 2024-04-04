39bc34bade5ffedd25b9efaab720a6e9cc0e87cc
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2024-04-04 16:55:37 +1100

    Fix addlabels!

--- src/Layouts.jl
+++ src/Layouts.jl
@@ -1,4 +1,4 @@
-export addlabels!, OnePanel, TwoPanel, FourPanel
+export addlabels!, OnePanel, TwoPanel, FourPanel, SixPanel
 
 # * A set of consistent figure layouts
 
@@ -14,13 +14,17 @@ function FourPanel(args...; kwargs...)
     f = Figure(args...; size = (720, 720))
     return f
 end
+function SixPanel(args...; kwargs...)
+    f = Figure(args...; size = (720, 1080))
+    return f
+end
 
 function addlabels!(f, text = nothing; kwargs...)
     content = f.layout.content
     allowedblocks = [Axis, Axis3, PolarAxis, LScene]
     content = filter(x -> any(isa.([x.content], allowedblocks)), content)
     n = prod(length(content))
-    spans = [[c.cols, c.rows] for c in getfield.(content, :span)]
+    spans = [[c.rows, c.cols] for c in getfield.(content, :span)]
     idxs = sortperm(spans)
 
     if isnothing(text)
@@ -33,7 +37,7 @@ function addlabels!(f, text = nothing; kwargs...)
 
     for (i, l) in enumerate(content[idxs])
         span = l.span
-        Label(f[span.cols, span.rows, TopLeft()], halign = :left, valign = :bottom,
+        Label(f[span.rows, span.cols, TopLeft()], halign = :left, valign = :bottom,
               text = text[i],
               fontsize = 22, padding = (-5, 0, 5, 0), kwargs...)
     end
