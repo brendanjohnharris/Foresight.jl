77ff1458cd822732b94be0ed71397eed57666ee4
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2025-01-23 14:34:51 +1100

    Silence WGLMakieExt

--- ext/WGLMakieExt.jl
+++ ext/WGLMakieExt.jl
@@ -3,25 +3,25 @@ using WGLMakie
 using WGLMakie.Bonito
 using Foresight
 
-function saveit(file = "index.html", f)
-    open(file, "w") do io
-        println(io, """
-        <html>
-            <head>
-            </head>
-            <body>
-        """)
-        Page(exportable = true, offline = true)
-        app = App() do
-            DOM.div(f)
-        end
-        show(io, MIME"text/html"(), app)
-        # or anything else from Bonito, or that can be displayed as html:
-        println(io, """
-            </body>
-        </html>
-        """)
-    end
-end
+# function saveit(file = "index.html", f)
+#     open(file, "w") do io
+#         println(io, """
+#         <html>
+#             <head>
+#             </head>
+#             <body>
+#         """)
+#         Page(exportable = true, offline = true)
+#         app = App() do
+#             DOM.div(f)
+#         end
+#         show(io, MIME"text/html"(), app)
+#         # or anything else from Bonito, or that can be displayed as html:
+#         println(io, """
+#             </body>
+#         </html>
+#         """)
+#     end
+# end
 
 end # module
